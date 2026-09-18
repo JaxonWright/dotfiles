5daaeed5dcdc314eb0f1e221de5c2dbf769d23b0
Jaxon Wright <jaxonhwright@gmail.com>
2026-09-18 19:13:33 -0400

    hypr: when opening discord or steam, do not focus them

--- hypr/.config/hypr/windows.lua
+++ hypr/.config/hypr/windows.lua
@@ -2,10 +2,10 @@
 
 -- Dedicated workspaces for apps.
 o.window("brave-browser", { workspace = "1" })
-o.window("discord", { workspace = "2 silent" })
+o.window("discord", { workspace = "2 silent", no_initial_focus = true })
 o.window("fastpotify", { workspace = "3 silent" })
 
 -- Steam: tiled on workspace 4.
-o.window({ class = "^(steam)$" }, { workspace = "4 silent", tile = true })
+o.window({ class = "^(steam)$" }, { workspace = "4 silent", tile = true, no_initial_focus = true })
 o.window({ class = "steam", title = "Steam" }, { tile = true })
 o.window({ class = "steam", title = "Friends List" }, { tile = true })
